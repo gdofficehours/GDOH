782a4398b0a371f7e93fe04f7faae39f273e5b8c
Peter Brinson <brinson@usc.edu>
2026-08-07 17:30:30 -0700

    Sync from ASUS14 at 2026-08-07 17:30

--- site/quartz.layout.ts
+++ site/quartz.layout.ts
@@ -25,10 +25,6 @@ export const defaultContentPageLayout: PageLayout = {
       component: Component.ArticleTitle(),
       condition: (page) => page.fileData.slug !== "index",
     }),
-    Component.ConditionalRender({
-      component: Component.ContentMeta(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
     Component.TagList(),
   ],
   left: [
@@ -85,7 +81,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
